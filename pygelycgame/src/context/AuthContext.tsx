155c07fd1e59e2a42a723de5ff1cf10a8bd583c0
Yinna Gomez <yinngomz186@gmail.com>
2025-11-09 13:30:54 -0500

    Configurar frontend para Vercel

--- pygelycgame/src/context/AuthContext.tsx
+++ pygelycgame/src/context/AuthContext.tsx
@@ -18,8 +18,9 @@ interface AuthContextType {
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-const API_URL = 'http://localhost:3001/api';
+//const API_URL = 'http://localhost:3001/api';
 //const API_URL = 'http://172.27.20.185:3001/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
